99e9f844ae05510a854e0905a49ffa6334814997
Brian Gershon <briangershon@users.noreply.github.com>
2024-11-02 22:11:34 -0700

    Port to vite and vitest. Upgrade to Node v20

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "vitest";
+import { helloWorld } from "./index";
+
+describe("index", () => {
+  test("has default message", async () => {
+    expect(helloWorld()).toEqual("Hello World!");
+  });
+});
